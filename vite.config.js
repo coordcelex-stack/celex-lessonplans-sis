72b8414203dd723495c3733ca8d5244bea4798d6
coordcelex-stack <coord.celex@uvp.mx>
2026-08-07 22:25:26 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+// rebuild 2026-08-07
+import { defineConfig } from 'vite'import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
